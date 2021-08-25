a60eface67d78c7e67b7ac62a34baa52dc5c6baa
Artem Dobikov <soadob14@gmail.com>
2021-08-25 14:03:40 +0300

    task_05_06 create_models

--- cute_web_application/product/models.py
+++ cute_web_application/product/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    id = models.BigAutoField(primary_key=1)
+    name = models.TextField(default='name')
+    description = models.TextField(default='description')
+    price = models.IntegerField(default=0)
+    value = models.IntegerField(default=0)
+    category = models.TextField(default='category')
+    material = models.TextField(default='material')
+    color = models.TextField(default='color')
+    manufacturer = models.TextField(default='manufacturer')
+    link = models.TextField(default='https://static.onlinetrade.ru/img/items/b/1348575_1.jpg')
+
+    def __str__(self):
+        return '{}'.format(self.name)
--- cute_web_application/product/views.py
+++ cute_web_application/product/views.py
@@ -1,10 +1,12 @@
 from django.http import request
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from .models import Product
 import os
 # Create your views here.
 def product(request):
     return HttpResponse("<h1>Вы в приложении product</h1>")
 
-def product1(request):
-    return render(request, 'site.html')
+def detail(request, product_id):
+    products_code = Product.objects.get(id=product_id)
+    return render(request, 'product.html', context={'product': products_code})
