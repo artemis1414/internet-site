9f68c924340576a629d5d8779fb8a0bfbbf99514
Artem Dobikov <soadob14@gmail.com>
2021-08-30 00:08:34 +0300

    fix task_8

--- cute_web_application/core/urls.py
+++ cute_web_application/core/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.registration, name='core'),
+    path('', views.signinsystem, name='core'),
+    path('registration/', views.registration, name='registration')
 ]
--- cute_web_application/core/views.py
+++ cute_web_application/core/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def registration(request):
-    return render(request, 'form.html')
+    return render(request, 'form_registration.html')
+
+def signinsystem(request):
+    return render(request, 'core/form.html')
