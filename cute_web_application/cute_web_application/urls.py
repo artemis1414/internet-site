bbdf03730e2ee8dc2805c8b1959ff17fb6c069d3
Artem Dobikov <soadob14@gmail.com>
2021-08-29 23:56:28 +0300

    create pattern form task 8

--- cute_web_application/cute_web_application/urls.py
+++ cute_web_application/cute_web_application/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('product/', include('product.urls')),
+    path('core/', include('core.urls'))
 ]
