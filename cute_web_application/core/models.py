2db1d5edf27a8065765bb38f6aec9b0926029d1e
Artem Dobikov <soadob14@gmail.com>
2021-09-09 12:26:43 +0300

    create_forms

--- cute_web_application/core/models.py
+++ cute_web_application/core/models.py
@@ -4,3 +4,4 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     id = models.BigAutoField(primary_key=True)
     phone = models.DecimalField(max_digits=11, decimal_places=0, default=88888888888, help_text='Номер телефона')
+
--- cute_web_application/core/views.py
+++ cute_web_application/core/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
+from django.core.exceptions import ValidationError
+from .forms import SignUpForm
 
 # Create your views here.
 
-def registration(request):
-    return render(request, 'form_registration.html')
+def sign_up(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            pass
+        else:
+            return ValidationError
+    form = SignUpForm()
+    data = {'form': form}
+    return render(request, 'form_registration.html', data)
 
-def signinsystem(request):
+def sign_in(request):
     return render(request, 'core/form.html')
