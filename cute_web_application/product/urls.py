5703306d07589312d0a13082e39c081a272f87ae
Artem Dobikov <soadob14@gmail.com>
2021-10-12 15:33:57 +0300

    added category

--- cute_web_application/product/urls.py
+++ cute_web_application/product/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.product, name='product'),
-    path('<int:product_id>/', views.detail, name='detail'),
+    path('category/', views.product, name='product'),
+    path('category/<int:category_id>/item<int:product_id>/', views.detail, name='detail'),
+    path('category/<int:category_id>/', views.catalog, name='catalog'),
 ]
--- cute_web_application/product/views.py
+++ cute_web_application/product/views.py
@@ -1,14 +1,14 @@
 from django.http import request
 from django.http.response import HttpResponse
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Category
 import os
 # Create your views here.
 def product(request):
-    list_product = Product.objects.all()
-    return render(request, 'product/product_base.html', context={'product': list_product})
+    list_category = Category.objects.all()
+    return render(request, 'category.html', context={'category': list_category})
 
-def detail(request, product_id):
+def detail(request, category_id, product_id):
     products_code = Product.objects.get(id=product_id)
     sizes_dict = Product.objects.filter(id=product_id).values()[0]
     sizes = []
@@ -16,5 +16,8 @@ def detail(request, product_id):
         sizes.append((f'{i}', sizes_dict[f'size_{i}_value']))
     return render(request, 'product.html', context={'product': products_code, 'sizes': sizes, })
 
-def catalog(request, product_id):
-    return render(request, 'product.html')
+def catalog(request, category_id):
+    print(category_id)
+    list_product = Product.objects.filter(category_id=category_id)
+    return render(request, 'product/product_base.html', context={'product': list_product})
+
